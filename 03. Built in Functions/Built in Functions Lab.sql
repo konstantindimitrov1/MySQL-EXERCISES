825d6f3a8da48b36b8e3c0cabdb35e2b26a65b70
Konstantin Dimitrov <103933804+konstantindimitrov1@users.noreply.github.com>
2022-07-16 11:17:08 +0300

    Add files via upload

---
+++ 03. Built in Functions/Built in Functions Lab.sql
@@ -0,0 +1,44 @@
+USE `book_library`;
+
+SELECT 
+    `title`
+FROM
+    `books`
+WHERE
+    SUBSTRING(`title`, 1, 3) = 'The'
+ORDER BY `id`;
+
+SELECT 
+    CONCAT('***', SUBSTRING(`title`, 4)) AS 'title'
+FROM
+    `books`
+WHERE
+    SUBSTRING(`title`, 1, 3) = 'The'
+ORDER BY `id`;
+
+SELECT 
+    REPLACE(`title`, 'The', '***') AS 'title'
+FROM
+    `books`
+WHERE
+    `title` LIKE ('The%')
+ORDER BY `id`;
+
+SELECT 
+    ROUND(SUM(`cost`), 2) AS 'cost'
+FROM
+    `books`;
+    
+SELECT 
+    CONCAT_WS(' ', `first_name`, `last_name`) AS 'Full Name',
+    TIMESTAMPDIFF(DAY, `born`, `died`) AS 'Days Lived'
+FROM
+    `authors`;
+    
+SELECT 
+    `title`
+FROM
+    `books`
+WHERE
+    `title` LIKE ('%Harry Potter%')
+ORDER BY `id`;
